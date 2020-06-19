097adfbeb21510cf6129277963a23ae50b74e198
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-19 12:39:50 +0100

    the output enjoy

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
         <div class="text-center" style="margin: 20px 0px 20px 0px;">
-            <a href="https://www.mynotepaper.com/" target="_blank"><img src="https://i.imgur.com/hHZjfUq.png"></a><br>
-            <span class="text-secondary">Laravel & Vue CRUD Single Page Application (SPA) Tutorial</span>
+            <a href="https://github.com/Medhanini" target="_blank"><img src="https://avatars2.githubusercontent.com/u/42269173?s=400&u=c4e0f0558595a65120e0b3ecaf12f6dca71981f7&v=4"></a><br>
+            <span class="text-secondary">Laravel & Vue CRUD Single Page Application (SPA) </span>
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
